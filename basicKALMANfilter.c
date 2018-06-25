90e9dc46f62f61b21df0ced06ef16a80c45b2c4a
Dhruv Patel <30570613+Dhruv2012@users.noreply.github.com>
2018-06-25 20:13:15 +0530

    Update basicKALMANfilter.c
    comments added

--- basicKALMANfilter.c
+++ basicKALMANfilter.c
@@ -1,14 +1,15 @@
 #include "basicKALMANfilter.h"
 
 void kalman_init(Kalman *kf,float qq,float rr,float pp,float initial_estimate){
-  kf->q=qq;  
-  kf->r=rr;
+  kf->q=qq;                          // kf kalman struct
+                                     // x is estimate of sensor reading or input for the filter
+  kf->r=rr;                           
   kf->p=pp;
   kf->x=initial_estimate;
 }
 void update_kalman(Kalman *kf,float measured_value){
-  kf->p=kf->p+kf->q;
-  kf->k=kf->p/(kf->p + kf->r);
-  kf->x=kf->x+kf->k*(measured_value-kf->x);
-  kf->p=(1-kf->k)*kf->p;    
+  kf->p=kf->p+kf->q;                        //r is the error in measurement
+  kf->k=kf->p/(kf->p + kf->r);              // k is the kalman gain
+  kf->x=kf->x+kf->k*(measured_value-kf->x); //p is the error in estimate
+  kf->p=(1-kf->k)*kf->p;                    //q process noise covariance
 } 
